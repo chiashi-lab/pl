72d5c1e3a408936961cc06dad0cdcfb407f6026f
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-05-07 19:35:55 +0900

    updated

--- src/main.py
+++ src/main.py
@@ -4,14 +4,9 @@ from thorlab import motor
 import time
 from pywinauto.application import Application
 
-def pid(targetpower, measuredpower, kp, ki, kd, dt, integral, prev_error):
-    error = targetpower - measuredpower
-    integral = integral + error * dt
-    derivative = (error - prev_error) / dt
-    output = kp * error + ki * integral + kd * derivative
-    prev_error = error
-    return output, integral, prev_error
-
+def control_power(targetpower):
+    nowpower = ophircom.get_power()
+    nowndstep = motor.get_position()
 
 if __name__ == "__main__":
     app = Application(backend="win32").start(r"C:\Users\maruk\Desktop\Jobin Yvon\SDK\Examples\C++\FilterWheel\Release\FWExample.exe", timeout=10)
