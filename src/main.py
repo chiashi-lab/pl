94d285263c7194a4726fad1378fb4a50cba1ee60
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-05-08 16:26:39 +0900

    updated

--- src/main.py
+++ src/main.py
@@ -6,18 +6,22 @@ from pywinauto.application import Application
 import func
 
 def control_power(targetpower,eps=0.001):
-    time.sleep(1)
-    nowndstep = motor.get_position()
-    ratio = func.step2ratio(nowndstep)
-    nowpower = ratio * ophircom.get_power()
+    while (True):
+        time.sleep(2)
+        nowndstep = motor.get_position()
+        ratio = func.step2ratio(nowndstep)
+        nowpower = ratio * ophircom.get_power()
+        print("Current power: ", nowpower)
+        print("Target power: ", targetpower)
 
-    if nowpower < targetpower - eps or targetpower + eps < nowpower:
-        byratio = targetpower / nowpower
-        byratio = ratio * byratio
-        tostep = func.ratio2step(byratio)
-        motor.move_to_position(tostep)
-    else:
-        print("Already at target power")
+        if nowpower < targetpower - eps or targetpower + eps < nowpower:
+            byratio = targetpower / nowpower
+            byratio = ratio * byratio
+            tostep = func.ratio2step(byratio)
+            motor.move_to_position(tostep)
+        else:
+            print("Already at target power")
+            break
 
 if __name__ == "__main__":
     app = Application(backend="win32").start(r"C:\Users\maruk\Desktop\Jobin Yvon\SDK\Examples\C++\FilterWheel\Release\FWExample.exe", timeout=10)
