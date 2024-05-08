0b56d98cbecbd2aa45ba3fa701b44204999a4855
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-05-08 14:47:34 +0900

    updated add funcpy

---
+++ src/func.py
@@ -0,0 +1,30 @@
+import numpy as np
+def optical_density_step(step):
+    if step <= 1.6e7:
+        return (step- 0.7e7) * 2 /0.9e7
+    else:
+        return 2
+
+def transmisson(od):
+    """
+    od: optical density
+    """
+    return 10 ** (-od)
+
+def func(x, a, b, c, d):
+    return -1 * a * np.exp(b * (x-c)) + d
+
+def step2ratio(step):
+    """
+    step: step number
+    """
+    return transmisson(optical_density_step(step)) * func(step, 2.92800423e-01, 2.10349362e-07, 9.10476001e+06, 5.38950106e+00)
+
+def inverse_func(y, a, b, c, d):
+    return -1 * a * np.log((y + b) / c) + d
+
+def ratio2step(ratio):
+    """
+    step: step number
+    """
+    return inverse_func(ratio, 1.90691015e+06, 5.73903423e-03, 3.77973757e-02, 1.64051423e+07)
--- src/main.py
+++ src/main.py
@@ -3,10 +3,16 @@ from ophircom import ophircom
 from thorlab import motor
 import time
 from pywinauto.application import Application
+import func
 
-def control_power(targetpower):
+def control_power(targetpower,eps=0.001):
     nowpower = ophircom.get_power()
     nowndstep = motor.get_position()
+
+    if nowpower < targetpower - eps:
+    elif nowpower > targetpower + eps:
+    else:
+        print("Already at target power")
 
 if __name__ == "__main__":
     app = Application(backend="win32").start(r"C:\Users\maruk\Desktop\Jobin Yvon\SDK\Examples\C++\FilterWheel\Release\FWExample.exe", timeout=10)
