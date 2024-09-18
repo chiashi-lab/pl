d838eb2ea28b8b81d50a1e4adb6bc3fb3e727a0e
kykougaku <6489550+kykougaku@users.noreply.github.com>
2024-09-18 17:55:14 +0900

    changed easylaser to class-style

--- src/easylaser_window.py
+++ src/easylaser_window.py
@@ -10,68 +10,79 @@ from shutter import shutter
 import config
 import sys
 
+class Application(tkinter.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.master.geometry("400x300")
+        self.master.title(u"レーザー狙い撃ち君")
+        self.create_widgets()
+    
+    def create_widgets(self):
+        self.label_wavelength = tkinter.Label(text=u'波長')
+        self.label_wavelength.place(x=30, y=10)
+        self.entry_wavelength = tkinter.Entry(width=7)
+        self.entry_wavelength.insert(tkinter.END, '500')
+        self.entry_wavelength.place(x=140, y=10)
+        self.unit_wavelength = tkinter.Label(text=u'nm')
+        self.unit_wavelength.place(x=200, y=10)
 
-def choonepower(targetpower, centerwavelength, wavelenghwidth):
-    laserchoone.change_lwbw(wavelength=centerwavelength, bandwidth=wavelenghwidth)
-    pid_control_power(targetpower=targetpower, wavelength=centerwavelength, powermeter=powermeter, NDfilter=NDfilter, eps=targetpower*config.EPSRATIO)
+        self.label_width = tkinter.Label(text=u'波長幅')
+        self.label_width.place(x=30, y=60)
+        self.entry_width = tkinter.Entry(width=7)
+        self.entry_width.insert(tkinter.END, '10')
+        self.entry_width.place(x=140, y=60)
+        self.unit_width = tkinter.Label(text=u'nm')
+        self.unit_width.place(x=200, y=60)
+        
+        self.label_power = tkinter.Label(text=u'目標パワー')
+        self.label_power.place(x=30, y=110)
+        self.entry_power = tkinter.Entry(width=7)
+        self.entry_power.insert(tkinter.END, '2')
+        self.entry_power.place(x=140, y=110)
+        self.unit_power = tkinter.Label(text=u'mW')
+        self.unit_power.place(x=200, y=110)
 
-def thredchoonepower(event):
-    thread1 = threading.Thread(target=choonepower(0.001*float(e3.get()), int(e1.get()), int(e2.get())))
-    thread1.start()
+        self.set_button = tkinter.Button(text=u'セット', width=20)
+        self.set_button.bind("<1>", self.call_choonepower)
+        self.set_button.place(x=20, y=200)
 
+        self.init_button = tkinter.Button(text=u'初期化', width=20)
+        self.init_button.bind("<1>", self.call_init)
+        self.init_button.place(x=200, y=200)
+
+    def call_init(self, event):
+        thread2 = threading.Thread(target=self.initialize())
+        thread2.start()
+
+    def call_choonepower(self, event):
+        thread1 = threading.Thread(target=self.choonepower(0.001*float(self.entry_power.get()), int(self.entry_wavelength.get()), int(self.entry_width.get())))
+        thread1.start()
+
+    def initialize(self):
+        self.flipshut = FlipMount()
+        self.flipshut.close()
+        self.shut = shutter(config.SHUTTERCOMPORT)
+        self.shut.close(2)
+
+        self.laserchoone = superchrome()
+
+        self.NDfilter = ThorlabStage(home=True)
+        self.NDfilter.move_to(0, block=True)
+        print(f"stage is at {self.NDfilter.get_position()}")
+
+        self.flipshut.open()
+        self.shut.open(2)
+
+        self.powermeter = ophircom()
+        self.powermeter.open()
+        self.powermeter.set_range(4)
+
+    def choonepower(self, targetpower, centerwavelength, wavelenghwidth):
+        self.laserchoone.change_lwbw(wavelength=centerwavelength, bandwidth=wavelenghwidth)
+        pid_control_power(targetpower=targetpower, wavelength=centerwavelength, powermeter=self.powermeter, NDfilter=self.NDfilter, eps=targetpower*config.EPSRATIO)
 
 if __name__ == "__main__":
-    flipshut = FlipMount()
-    flipshut.close()
-    shut = shutter(config.SHUTTERCOMPORT)
-    shut.close(2)
-
-    laserchoone = superchrome()
-
-
-    NDfilter = ThorlabStage(home=True)
-    NDfilter.move_to(0, block=True)
-    print(f"stage is at {NDfilter.get_position()}")
-
-    flipshut.open()
-    shut.open(2)
-
-    powermeter = ophircom()
-    powermeter.open()
-    powermeter.set_range(4)
-
-
     root = tkinter.Tk()
-    root.title(u"レーザー狙い撃ち君")
-    root.geometry("400x300")
-
-    l1 = tkinter.Label(text=u'波長')
-    l1.place(x=30, y=10)
-    e1 = tkinter.Entry(width=7)
-    e1.insert(tkinter.END, '500')
-    e1.place(x=140, y=10)
-    l1l = tkinter.Label(text=u'nm')
-    l1l.place(x=200, y=10)
-
-    l2 = tkinter.Label(text=u'波長幅')
-    l2.place(x=30, y=60)
-    e2 = tkinter.Entry(width=7)
-    e2.insert(tkinter.END, '10')
-    e2.place(x=140, y=60)
-    l2l = tkinter.Label(text=u'nm')
-    l2l.place(x=200, y=60)
-
-    l3 = tkinter.Label(text=u'目標パワー')
-    l3.place(x=30, y=110)
-    e3 = tkinter.Entry(width=7)
-    e3.insert(tkinter.END, '2')
-    e3.place(x=140, y=110)
-    l3l = tkinter.Label(text=u'mW')
-    l3l.place(x=200, y=110)
-
-    #ボタン
-    Button1 = tkinter.Button(text=u'セット', width=20)#, command=lambda: go_pl())
-    Button1.bind("<1>", thredchoonepower)
-    Button1.place(x=20, y=200)
-
-    root.mainloop()
+    app = Application(master=root)
+    app.mainloop()
