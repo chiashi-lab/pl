892a6315381c73bedcd761f8c997e9db7d485e34
kykougaku <6489550+kykougaku@users.noreply.github.com>
2025-01-02 16:49:44 +0900

    :wrench: change func of wavelength2ratio

--- src/func.py
+++ src/func.py
@@ -2,7 +2,7 @@ def linear(x:float, a:float, b:float) -> float:
     return a * x + b
 
 def wavelength2ratio(wavelength: int) -> float:
-    return linear(float(wavelength), -0.0039, 9.3265)
+    return linear(float(wavelength), -0.0165, 1.677)
 
 def waittime4exposure(exposure:float)->float:
     return linear(float(exposure), 1.1, 2.0)
