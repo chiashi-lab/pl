10b9ab2b909d5b43c388ca7537cd45bb18b49000
kykougaku <6489550+kykougaku@users.noreply.github.com>
2025-04-28 21:09:05 +0900

    :memo: add test code

--- src/test/test_power.py
+++ src/test/test_power.py
@@ -28,13 +28,16 @@ def get_wavelength_power_relation(save_path):
     tisp = zaber_linear_actuator()
     spectrometer = thorlabspectrometer()
 
-    wavelengthlist = np.arange(700, 900+10, 10)
+    wavelengthlist = np.arange(700, 850+10, 10)
 
     measured_power_list = []
     predicted_power_list = []
+    tisp_position_list = []
     for wavelength in wavelengthlist:
         print(f"measuring at {wavelength} nm")
         pid_control_wavelength(wavelength, tisp, spectrometer, logger)
+        tisp_position = tisp.get_position()
+        print(f"tisp position: {tisp_position}")
         time.sleep(10)
         measured_power = powermeter.get_latestdata()
         predicted_power = func.ndstep2ratio(0) * measured_power
@@ -43,11 +46,13 @@ def get_wavelength_power_relation(save_path):
 
         measured_power_list.append(measured_power)
         predicted_power_list.append(predicted_power)
+        tisp_position_list.append(tisp_position)
 
     df = pd.DataFrame({
         "wavelength": wavelengthlist,
         "measured_power": measured_power_list,
-        "predicted_power": predicted_power_list
+        "predicted_power": predicted_power_list,
+        "tisp_position": tisp_position_list
     })
     df.to_csv(os.path.join(save_path, "wavelength_power_relation.csv"), index=False)
 
