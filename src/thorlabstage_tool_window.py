beb3de978ac991cfb2f20aa9a69f54911ce461ee
kykougaku <6489550+kykougaku@users.noreply.github.com>
2025-11-04 16:46:47 +0900

    初期実装

--- src/thorlabstage_tool_window.py
+++ src/thorlabstage_tool_window.py
@@ -3,22 +3,19 @@ from tkinter import ttk
 import sys
 sys.coinit_flags = 2
 import threading
-from measurment import pid_control_power, pid_control_wavelength
-from driver.ophir import juno
+from measurment import pid_control_wavelength
 from driver.thorlab import ThorlabStage, FlipMount, thorlabspectrometer
 from driver.sigmakoki import shutter
 from driver.zaber import zaber_linear_actuator
 from logger import Logger
 import config
-from power_dict import PowerDict
-import matplotlib.pyplot as plt
 
 class Application(tkinter.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.master.geometry("500x500")
-        self.master.title(u"励起光照射制御")
+        self.master.title(u"NDフィルターステージ・TiSap波長切替ツール")
         self.create_widgets()
     
     def create_widgets(self):
@@ -31,17 +28,17 @@ class Application(tkinter.Frame):
         self.unit_wavelength = tkinter.Label(text=u'nm', state='disabled')
         self.unit_wavelength.place(x=200, y=60)
 
-        self.label_power = tkinter.Label(text=u'目標パワー',state='disabled')
-        self.label_power.place(x=30, y=160)
-        self.entry_power = tkinter.Entry(width=7)
-        self.entry_power.insert(tkinter.END, '1')
-        self.entry_power["state"] = "disabled"
-        self.entry_power.place(x=140, y=160)
-        self.unit_power = tkinter.Label(text=u'mW', state='disabled')
-        self.unit_power.place(x=200, y=160)
+        self.label_position = tkinter.Label(text=u'目標位置',state='disabled')
+        self.label_position.place(x=30, y=160)
+        self.entry_position = tkinter.Entry(width=10)
+        self.entry_position.insert(tkinter.END, '500000')
+        self.entry_position["state"] = "disabled"
+        self.entry_position.place(x=140, y=160)
+        self.unit_position = tkinter.Label(text=u'internal unit', state='disabled')
+        self.unit_position.place(x=220, y=160)
 
         self.set_button = tkinter.Button(text=u'セット', width=20)
-        self.set_button.bind("<1>", self.call_choonepower)
+        self.set_button.bind("<1>", self.call_choone_wavelength_stage)
         self.set_button.place(x=20, y=220)
         self.set_button["state"] = tkinter.DISABLED
 
@@ -68,23 +65,23 @@ class Application(tkinter.Frame):
         thread2 = threading.Thread(target=self.initialize)
         thread2.start()
 
-    def call_choonepower(self, event):
+    def call_choone_wavelength_stage(self, event):
         if self.set_button["state"] == tkinter.DISABLED:
             return
         self.set_button["state"] = tkinter.DISABLED
         try:
-            power = float(self.entry_power.get()) * 0.001
+            position = int(self.entry_position.get())
             wavelength = int(self.entry_wavelength.get())
         except Exception as e:
             print(e)
             self.msg.set(f"値を正しく入力してください\n{e}")
             self.set_button["state"] = tkinter.NORMAL
             return
-        if power < 0.0 or power > 4.0 or wavelength < 700 or wavelength > 850:
+        if position < config.KINESISSTAGEMINLIMIT or position > config.KINESISSTAGEMAXLIMIT or wavelength < 700 or wavelength > 850:
             self.msg.set("正しい値を入力して下さい")
             self.set_button["state"] = tkinter.NORMAL
             return
-        thread1 = threading.Thread(target=self.choonepower, args=(power, wavelength))
+        thread1 = threading.Thread(target=self.choone_wavelength_stage, args=(position, wavelength))
         thread1.start()
 
     def initialize(self):
@@ -96,16 +93,9 @@ class Application(tkinter.Frame):
             self.flipshut.close()
             self.logger.log("flipshut is closed")
 
-            self.mypowerdict = PowerDict()
-
             self.NDfilter = ThorlabStage(home=True)
             self.NDfilter.move_to(0, block=True)
             self.logger.log(f"stage is at {self.NDfilter.get_position()}\n")
-
-            self.powermeter = juno()
-            self.powermeter.open()
-            self.powermeter.set_range(0)
-            self.logger.log("powermeter is opened")
 
             self.tisp = zaber_linear_actuator()
             self.logger.log("TiSap actuator is initialized")
@@ -124,13 +114,13 @@ class Application(tkinter.Frame):
         self.entry_wavelength["state"] = "normal"
         self.label_wavelength["state"] = "normal"
         self.unit_wavelength["state"] = "normal"
-        self.entry_power["state"] = "normal"
-        self.label_power["state"] = "normal"
-        self.unit_power["state"] = "normal"
+        self.entry_position["state"] = "normal"
+        self.label_position["state"] = "normal"
+        self.unit_position["state"] = "normal"
         self.pb.stop()
         self.msg.set("初期化完了．値を設定してセットを押してください")
 
-    def choonepower(self, targetpower, centerwavelength):
+    def choone_wavelength_stage(self, targetposition, centerwavelength):
         self.msg.set("波長の切替とパワーの調整中...")
         self.pb.start(10)
         try:
@@ -138,20 +128,18 @@ class Application(tkinter.Frame):
 
             self.logger.log(f"start wavelength control at {centerwavelength}")
             pid_control_wavelength(targetwavelength=centerwavelength, TiSap_actuator=self.tisp, spectrometer=self.spectrometer, logger=self.logger)
-            self.logger.log(f"start power control at {centerwavelength} for {targetpower}")
-            poslog = pid_control_power(targetpower=targetpower, powermeter=self.powermeter, NDfilter=self.NDfilter, eps=targetpower*config.EPSRATIO, logger=self.logger, NDinitpos=self.mypowerdict.get_nearest(centerwavelength, targetpower))
-            self.mypowerdict.add(centerwavelength, targetpower, self.NDfilter.get_position())
-            #plt.plot(poslog)
-            #plt.show()
+            self.logger.log(f"start power control at {centerwavelength} for {targetposition}")
+            self.NDfilter.move_to(targetposition, block=True)
+            self.logger.log(f"NDfilter is at {self.NDfilter.get_position()}")
         except Exception as e:
             print(e)
-            self.msg.set(f"波長の切替とパワーの調整に失敗しました\n{e}")
+            self.msg.set(f"波長の切替とNDフィルター位置の調整に失敗しました\n{e}")
             self.pb.stop()
             self.set_button["state"] = tkinter.NORMAL
             return
         self.pb.stop()
         self.set_button["state"] = tkinter.NORMAL
-        self.msg.set(f"手動シャッター操作で照射してください.波長:{centerwavelength}nm,パワー:{targetpower*1000}mW")
+        self.msg.set(f"手動シャッター操作で照射.波長:{centerwavelength}nm, NDフィルター位置:{targetposition}internal unit")
 
 if __name__ == "__main__":
     root = tkinter.Tk()
